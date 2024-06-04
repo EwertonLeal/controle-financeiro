76349d1e9fe47fae6e613c17e2b501c45c619d7b
EwertonLeal <ewertonolivaleal6@gmail.com>
2024-06-04 12:40:19 -0300

    configurações iniciais do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './shared/components/banner/banner.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CadastrarComponent } from './pages/cadastrar/cadastrar.component';
+import { FormWrapperComponent } from './shared/components/form-wrapper/form-wrapper.component';
+import { LoginFormComponent } from './shared/components/login-form/login-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RegisterFormComponent } from './shared/components/register-form/register-form.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    FooterComponent,
+    LoginComponent,
+    CadastrarComponent,
+    FormWrapperComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
