060e08c6099aeb94c87200f63ad40abbee6acfe3
EwertonLeal <ewertonolivaleal6@gmail.com>
2024-07-22 22:55:31 -0300

    estilização da tag main e chamada do componente login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { FooterComponentComponent } from './shared/footer-component/footer-component.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { BannerComponent } from './shared/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponentComponent
+    FooterComponentComponent,
+    LoginComponent,
+    SignUpComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
