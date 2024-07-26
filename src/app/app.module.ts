db421ee0d848a09a51859b0723044abca5466884
EwertonLeal <ewertonolivaleal6@gmail.com>
2024-07-26 09:47:01 -0300

    instalando o firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { RouterModule } from '@angular/router';
 import { FooterComponentComponent } from './shared/footer-component/footer-component.component';
 import { AppRoutingModule } from './app.routing';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { AngularFireModule  } from '@angular/fire/compat'
+import { AngularFireAuthModule  } from '@angular/fire/compat/auth'
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,18 @@ import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyD7I09SlmoMR41qvrQy6gBeXsJluxx4dxE",
+      authDomain: "controle-financeiro-bb9e8.firebaseapp.com",
+      projectId: "controle-financeiro-bb9e8",
+      storageBucket: "controle-financeiro-bb9e8.appspot.com",
+      messagingSenderId: "669706487449",
+      appId: "1:669706487449:web:f7a5c654f495415717da08",
+      measurementId: "G-DJ0Y8CKQKM"
+    }),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
